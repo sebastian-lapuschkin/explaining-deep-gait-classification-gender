f64711b2aebef36e274d75d89a3a9ffbcac814ab
Sebastian Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2021-03-25 11:29:41 +0100

    removing entry point for debugging

--- python/gait_experiments_batch_7.py
+++ python/gait_experiments_batch_7.py
@@ -6,16 +6,4 @@ Created on Fri Mar 12 15:01:45 2021
 """
 
 # Phase - 1
-#runfile('V:\Trainingswissenschaft\Horst\Gait - XAI - Gender\interpretable-deep-gait-injury\python\gait_experiments.py', args='-d ./data/Phase-2_GaitRec-Gutenberg-AIST_atMM -o ./output/Phase-2_GaitRec-Gutenberg-AIST_atMM.mat -s 3 -a CnnC3', wdir='V:\Trainingswissenschaft\Horst\Gait - XAI - Gender\interpretable-deep-gait-injury\python')
-import sys
-print(sys.argv)
-sys.argv += ['-d', './data/Phase-2_GaitRec-Gutenberg-AIST_atMM',
-             '-o', './output/Phase-2_GaitRec-Gutenberg-AIST_atMM.mat',
-             #'-d', './data/Phase-1_AIST_atMM',
-             '-s', '3',
-             '-a', 'CnnC3']
-
-print(sys.argv)
-
-#start debugging right here:
-import gait_experiments
+runfile('V:\Trainingswissenschaft\Horst\Gait - XAI - Gender\interpretable-deep-gait-injury\python\gait_experiments.py', args='-d ./data/Phase-2_GaitRec-Gutenberg-AIST_atMM -o ./output/Phase-2_GaitRec-Gutenberg-AIST_atMM.mat -s 3 -a CnnC3', wdir='V:\Trainingswissenschaft\Horst\Gait - XAI - Gender\interpretable-deep-gait-injury\python')
