74f9b60fbf152de790f4b6a044bd43ad82434cca
Sebastian Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2021-04-13 14:59:21 +0200

    improved demonstration

--- python/dataloadingtest.py
+++ python/dataloadingtest.py
@@ -4,13 +4,14 @@
 import sys
 print(sys.argv)
 # data inputs are given in order test val train (3 datasets) or test val/train (2 datasets). If a single dataset is given, splits are generated as per parameterization
-sys.argv += [#'-d', './data/Phase-1_AIST_atMM_8.mat ./data/Phase-1_GaitRec_atMM_8.mat ./data/Phase-1_Gutenberg_atMM_8.mat',
-            '-d', './data/Phase-1_AIST_atMM_8.mat',
+sys.argv += ['-d', './data/Phase-1_AIST_atMM_8.mat ./data/Phase-1_GaitRec_atMM_8.mat ./data/Phase-1_Gutenberg_atMM_8.mat',
+            #'-d', './data/Phase-1_AIST_atMM_8.mat',
              '-o', './testing/blabla.mat',
              '-s', '3', #should be ignored with multiple data entries given
              '-a', 'SvmLinearL2C1e0',
              '-me', 'retrain',
-             '-dt', '1',
+             #'-dt', None,
+             '-dt', '1'
              ]
 
 print(sys.argv)
